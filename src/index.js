afc1676a2e83706e5502cd32e73516994b54ec3e
lfjnascimento <lfjnascimento@gmail.com>
2021-11-07 01:36:23 -0400

    Change theme primary color

--- src/index.js
+++ src/index.js
@@ -2,14 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider  } from '@mui/material/styles';
 
 import App from './App';
-import Admin from './pages/Admin';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#359830'
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <CssBaseline />
-    <Admin />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider >
   </React.StrictMode>,
   document.getElementById('root')
 );
