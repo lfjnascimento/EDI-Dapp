702b6cdc05663d1ce7d29b81bbec9df73e400518
lfjnascimento <lfjnascimento@gmail.com>
2021-11-13 02:09:24 -0400

    Change PDF text e .gitignore

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,7 +1,6 @@
 import { jsPDF } from 'jspdf';
 
 export function generateDegreePDF(degree, courses){
-  console.log(degree)
   const course = courses.find((c) => c.id === degree.course_ID);
   const { student } = degree;
   const birthdate = new Date(student.birthdate?.toNumber());
@@ -20,20 +19,26 @@ export function generateDegreePDF(degree, courses){
   INSTITUTO INEXISTENTE DE EDUCAÇÃO DE MATO GROSSO
   criado pela Lei nº 000 de dezembro de 2021, publicado no D.O.U em 12/11/2021.`;
 
-  const middleText1 = `O Diretor Geral do Campus Cuiabá do Insituto Inexistente de Educação de Mato Grosso,\
-  no uso de suas atribuições e tendo em vista a conclusão do curso de ${course.name.toUpperCase()}, no ano de ${completionDate.getFullYear()},\
-  e a Colação de Grau em ${graduationDate.toLocaleDateString()} confere o Título de`;
+  const middleText1 = `O Diretor Geral do Campus Cuiabá do Insituto Inexistente de Educação de Mato Grosso, \
+no uso de suas atribuições e tendo em vista a conclusão do curso de ${course.name.toUpperCase()}, no ano de ${completionDate.getFullYear()}, \
+e a Colação de Grau em ${graduationDate.toLocaleDateString()} confere o Título de`;
   
-  const middleText2 = `${course.degreeType.toUpperCase()} em ${course.name.toUpperCase()} a`;
+  const middleText2 = `${course.degreeType} em ${course.name.toUpperCase()} a`;
   
   const middleText3 = `${student.name.toUpperCase()}`;
   
-  const middleText4 = `nascido em ${birthdate.toLocaleDateString()},\
-  nacionalidade ${student.nationality.toUpperCase()}, Natural de ${student.birthplace.toUpperCase()},\
-  RG nº ${student.RG_number} - ${student.RG_agency}/${student.RG_UF}, outorga-lhe o presente Diploma\
-  a fim de que possa gozar todos os direitos e prerrogativas legais.`;
+  const middleText4 = `nascido em ${birthdate.toLocaleDateString()}, \
+nacionalidade ${student.nationality.toUpperCase()}, Natural de ${student.birthplace.toUpperCase()}, \
+RG nº ${student.RG_number} - ${student.RG_agency}/${student.RG_UF}, outorga-lhe o presente Diploma \
+a fim de que possa gozar de todos os direitos e prerrogativas legais.`;
 
   const middleText5 = `Cuiabá-MT, ${issueDate.toLocaleDateString()}`;
+
+  const backText1 = `Diploma Registrado sob nº ${degree.register_number}.\n
+Processo nº ${degree.process_number}.\n
+Nos termos da Lei nº 000/99, artigo 11, em ${issueDate.toLocaleDateString()}.`
+
+  const backText2 =`Identificação eletrônica: ${degree.ID} `
 
 
   const urlIESImage = 'https://i.imgur.com/VYhNaTQ.png';
@@ -48,10 +53,16 @@ export function generateDegreePDF(degree, courses){
 
   doc.setFontSize(22);
   doc.text(middleText2, 15, 10, {align: 'center', maxWidth: 25});
+  doc.text(middleText3, 15, 12, {align: 'center', maxWidth: 25});
 
   doc.setFontSize(14);
-  doc.text(middleText4, 15, 14, {align: 'center', maxWidth: 25});
+  doc.text(middleText4, 15, 13, {align: 'center', maxWidth: 25});
   doc.text(middleText5, 15, 18, {align: 'center', maxWidth: 25});
+  
+  doc.addPage()
+  doc.text(backText1, 10, 5, {align: 'center', maxWidth: 25});
 
-  doc.output('pdfobjectnewwindow');
+  doc.text(backText2, 2, 20, {maxWidth: 25});
+
+  doc.output('pdfobjectnewwindow')
 }
